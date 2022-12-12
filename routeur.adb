d58bff5a1e11320c742befdd94f23ad988514f26
yguirene <120386159+yguirene@users.noreply.github.com>
2022-12-12 15:44:09 +0100

    Update routeur.adb

--- routeur.adb
+++ routeur.adb
@@ -9,13 +9,55 @@ procedure Routeur is
 
      -- Initialise les options du programme par défaut
      procedure Traiter_option(Fich_Table : out String; Fich_Paquets : out String; Fich_Resultats : out String; Politique : out T_Politique; Stat : out Boolean) is
+     i:Integer;
+     
      begin
           Politique := FIFO;
           Stat := true;
           Fich_Table := "table.txt";
           Fich_Paquets := "paquets.txt";
           Fich_Resultats := "resultats.txt";
-     end Initialiser_option;
+          
+          i:=1;
+          while i <= Argument_Count loop
+               
+               if Argument(i) = "-c" then
+                    i:=i+1;
+                    Taille_Cache:= Argument(i);
+               elsif Argument(i)= "-P" then
+                    i:= i+1;
+                    
+                    if Argument(i) = "FIFO" then
+                         Politique := FIFO;
+                    elsif Argument(i) = "LRU" then
+                         Politique := LRU;
+                    elsif Argument(i) = "LFU" then
+                         Politique := LFU;
+                    else
+                         Put("hh");
+                    end if;
+               elsif Argument(i)= "-p" then
+                    Fich_Paquets:= Argument(i);
+               elsif Argument(i)= "-S" then
+                    Stat:= True;
+               elsif Argument(i)= "-s" then
+                    Stat:= False;
+               elsif Argument(i)= "-t" then
+                    i:=i+1;
+                    Fich_Tabl:=Argument(i);
+               elsif Argument(i)= "-r" then
+                    i:=i+1;
+                    Fich_Resultats := Argument(i);
+               else
+                    Put("hh");
+               end if;     
+               
+               
+               i:= i+1;
+          end loop;
+               
+               
+     end Traiter_Option;
      
      Fich_Table, Fich_Paquets, Fich_Resultats : String;
      Stat : Boolean;
