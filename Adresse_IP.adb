62f159bc655acd480ea6f618ded1e734c2aab8d3
tom38110 <tom.audard@gmail.com>
2022-12-13 17:43:27 +0100

    création et mise en place des programmes de pour lire et comparer des adresses IP

--- Adresse_IP.adb
+++ Adresse_IP.adb
@@ -1,21 +1,24 @@
 with Ada.Text_IO;            use Ada.Text_IO;
 with Ada.Integer_Text_IO;    use Ada.Integer_Text_IO;
 
+
 package body Adresse_IP is
+
+     
 
      function Conv_Ligne_IP (ligne: in string) return T_Adresse_IP is
 
           Taille_Ligne: Integer;      -- la taille de la ligne
           T1: Integer;
           T2: Integer;
-          Valeur: constant Integer;   -- espace alloué à la conversion en entier
+          Valeur: constant Integer;   -- espace allouï¿½ ï¿½ la conversion en entier
 
      begin
           T1:=1;
 
-          Taille_Ligne := lenght(ligne);  -- on compte le nombre de caractère dans le chaîne
+          Taille_Ligne := lenght(ligne);  -- on compte le nombre de caractï¿½re dans le chaï¿½ne
 
-          for elm in (1..Taille_Ligne) loop   -- on parcours la chaîne
+          for elm in (1..Taille_Ligne) loop   -- on parcours la chaï¿½ne
                if ligne(elm) = '.' then       -- si on tombe sur un . on le remplace par un espace
 
                     T2:=elm-1;
@@ -32,3 +35,45 @@ package body Adresse_IP is
 
 
      end Conv_Ligne_IP;
+
+
+     function Lire_Adresse_IP (Entree : File_Type) return T_Adresse_IP is 
+          c: character;
+          val:Integer;
+          ad_ip:T_Adresse_IP;
+     begin
+          
+          for i in 1..4 loop
+               Get(Entree,val);
+               Get(Entree,c);
+               ad_ip := ad_ip*UN_OCTET + val;
+          end loop;
+
+     end Lire_Adresse_IP;
+
+
+     function Comp_Destination_Paquet(Destination:T_Adresse_IP; Masque: T_Adresse_IP; Paquet:T_Adresse_IP) return Boolean is 
+          val2:T_Adresse_IP;
+          T:Boolean;
+          M:T_Adresse_IP;
+          i:Integer;
+     begin
+          T:=True;
+          val2 := Paquet;
+          M := Masque;
+          i:=0;
+          while T=True and then i < 4 loop
+               if (val2 mod UN_OCTET) = (Destination mod UN_OCTET) then
+                    null;
+               else
+                    if (masque mod UN_OCTET) = 0 then
+                         val2:= val2 / UN_OCTET;
+                         Destination := Destination / UN_OCTET;
+                         M:= M / UN_OCTET;
+                    else
+                         T:=False;
+                    end if;
+               end if ;
+               i:=i+1;
+          end loop;
+     end Comp_Destination_Paquet;
--- Adresse_IP.ads
+++ Adresse_IP.ads
@@ -1,15 +1,29 @@
-generic
-
-
+with Ada.Text_IO;                  use Ada.Text_IO;
+with Ada.Integer_Text_IO;          use Ada.Integer_Text_IO;
+with Ada.Command_Line;             use Ada.Command_Line;
+with Ada.Strings;                  use Ada.Strings;
+with Ada.Strings.Unbounded;        use Ada.Strings.Unbounded;
+with Ada.Text_IO.Unbounded_IO;     use Ada.Text_IO.Unbounded_IO;
 
 package Adresse_IP is
 
-     type T_Adresse_IP is limited private;
+     type T_Adresse_IP is mod 2 ** 32;
+
+     UN_OCTET: constant T_Adresse_IP := 2 ** 8;
 
      -- Convertir une ligne (string) en Adresse IP (binaire)
      function Conv_Ligne_IP (ligne: in string) return T_Adresse_IP;
 
      -- Convertir une Adresse IP (binaire) en ligne (string)
      function Conv_IP_ligne (IP: in T_Adresse_IP) return String;
+
+     -- Lit l'adresse IP dans le fichier et la renvoie
+     function Lire_Adresse_IP (Entree : File_Type) return T_Adresse_IP;
+
+     -- Dis si un paquet correspond
+     function Comp_Destination_Paquet(Destination:T_Adresse_IP; Masque: T_Adresse_IP; Paquet:T_Adresse_IP) return Boolean;
+
+
+     
 
 end Adresse_IP;
