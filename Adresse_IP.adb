fce09990d6ca7a7ec4d3932d4a0a70af93114c15
yguirene <120386159+yguirene@users.noreply.github.com>
2022-12-14 14:47:49 +0100

    Update Adresse_IP.adb
    ajout de conv_IP_ligne et correction des erreurs des autres programmes + qques commentaires

--- Adresse_IP.adb
+++ Adresse_IP.adb
@@ -4,40 +4,47 @@ with Ada.Integer_Text_IO;    use Ada.Integer_Text_IO;
 
 package body Adresse_IP is
 
-     
+     -- Fonction qui a pour but de convertir une adresse IP (binaire) en une chaine de caractère composée de quatre 
+     -- entiers de la forme "n1.n2.n3.n4".
 
-     function Conv_Ligne_IP (ligne: in string) return T_Adresse_IP is
+     function Conv_IP_String (Adresse_IP: in T_Adresse_IP) return unbounded_string is
 
-          Taille_Ligne: Integer;      -- la taille de la ligne
-          T1: Integer;
-          T2: Integer;
-          Valeur: constant Integer;   -- espace allou� � la conversion en entier
+        type T_Adresse_IP is mod 2 ** 32;
+
+        UN_OCTET: constant T_Adresse_IP := 2 ** 8;       -- 256
+        IP: T_Adresse_IP;                                -- Adresse IP copie qui permet d'afficher l'adresse IP sans modifier l'origiinale
+        Str: unbounded_string;                           -- Adresse IP convertie sous forme de chaine de caractère
 
      begin
-          T1:=1;
+     
+        IP := Adresse_IP + 2 ** 8;
 
-          Taille_Ligne := lenght(ligne);  -- on compte le nombre de caract�re dans le cha�ne
+        for i in 1..4 loop                                                          -- on boucle 4 fois pour afficher toutes les valeurs dans une adresse IP
+            Str:= Str &  Integer'Image (((IP / UN_OCTET ** (4-i)) mod UN_OCTET));
+            while i/=4 loop                                                             -- permet d'afficher les "." juste après les 3 premières valeurs
+                Str:= Str & ".";
+            end loop;
+        end loop;
 
-          for elm in (1..Taille_Ligne) loop   -- on parcours la cha�ne
-               if ligne(elm) = '.' then       -- si on tombe sur un . on le remplace par un espace
-
-                    T2:=elm-1;
-                    Valeur:= Integer'Valeur(ligne(T1..T2));
-                    T1:=T2;
-
-               end if;
-          end loop;
-
-          Valeur:= Integer'Valeur(ligne);   -- conversion en entier
+        return Str;                                                                  -- on renvoie l'adresse IP convertie sous forme de chaine de caractère
+     end Conv_IP_String;
 
 
-
-
-
-     end Conv_Ligne_IP;
-
-
+     -- Fonction qui permet de lire une adresse IP dans un fichier Entree et la convertir en un type T_Adresse_IP
+    
      function Lire_Adresse_IP (Entree : File_Type) return T_Adresse_IP is 
+          c: character;                     -- permet de traiter le "."
+          val:Integer;                      -- la valeur (ni) qui va etre convertie en binaire
+          ad_ip:T_Adresse_IP;               -- l'adresse IP convertie en binaire
+     begin
+          
+          for i in 1..4 loop                -- pour chaque ni de l'adresse IP
+               Get(Entree,val);             -- on récupère la valeur de ni
+               Get(Entree,c);               -- on lit le "." pour éviter de stopper l'algorithme
+               ad_ip := ad_ip*UN_OCTET + val;       -- on construit l'adresse_IP à l'aide du schéma de Horner
+          end loop;
+          return ad_ip;                     -- on renvoie l'adresse IP convertie en binaire
+     end Lire_Adresse_IP;     function Lire_Adresse_IP (Entree : File_Type) return T_Adresse_IP is 
           c: character;
           val:Integer;
           ad_ip:T_Adresse_IP;
@@ -52,28 +59,34 @@ package body Adresse_IP is
      end Lire_Adresse_IP;
 
 
+     -- Fonction qui renvoie un booléen qui confirme ou non la compatibilité entre une Destination et un Paquet.
+
      function Comp_Destination_Paquet(Destination:T_Adresse_IP; Masque: T_Adresse_IP; Paquet:T_Adresse_IP) return Boolean is 
           val2:T_Adresse_IP;
           T:Boolean;
           M:T_Adresse_IP;
           i:Integer;
      begin
-          T:=True;
-          val2 := Paquet;
-          M := Masque;
+          T:=True;                          -- on initialise notre T à True 
+          val2 := Paquet;                   -- on créé une copie du paquet
+          M := Masque;                      -- on créé une copie du masque 
           i:=0;
-          while T=True and then i < 4 loop
-               if (val2 mod UN_OCTET) = (Destination mod UN_OCTET) then
+          while T=True and then i < 4 loop                      -- tant qu'on a bien égalité entre les valeurs de destination et de paquet et que l'on n'est pas arrivé à la fin
+               if (val2 mod UN_OCTET) = (Destination mod UN_OCTET) then -- si on a egalité entre les 2 ni on ne fait rien
                     null;
-               else
-                    if (masque mod UN_OCTET) = 0 then
-                         val2:= val2 / UN_OCTET;
+               else                                                 
+                    if (masque mod UN_OCTET) = 0 then               -- sinon on observe la valeur du masque associée, si elle est égale à 0 on ne fait rien
+                         val2:= val2 / UN_OCTET;                    -- on modifie les valeurs des copies afin de passer aux ni suivants
                          Destination := Destination / UN_OCTET;
                          M:= M / UN_OCTET;
                     else
-                         T:=False;
+                         T:=False;                                  -- on change T en False car on ne respecte pas la cohérence du cache.
                     end if;
                end if ;
-               i:=i+1;
+               i:=i+1;                      -- permet d'assurer la bonne terminaison de la boucle while à la fin de la comparaison
           end loop;
+          return T;
      end Comp_Destination_Paquet;
+          
+end Adresse_IP;
+          
