1c9dc4088da52449f288d7aaabe737f2fe7641f6
yguirene <120386159+yguirene@users.noreply.github.com>
2022-12-14 14:53:13 +0100

    Update Adresse_IP.ads
    changement des noms et supression des fonctions inutiles

--- Adresse_IP.ads
+++ Adresse_IP.ads
@@ -8,22 +8,15 @@ with Ada.Text_IO.Unbounded_IO;     use Ada.Text_IO.Unbounded_IO;
 package Adresse_IP is
 
      type T_Adresse_IP is mod 2 ** 32;
-
      UN_OCTET: constant T_Adresse_IP := 2 ** 8;
 
-     -- Convertir une ligne (string) en Adresse IP (binaire)
-     function Conv_Ligne_IP (ligne: in string) return T_Adresse_IP;
-
      -- Convertir une Adresse IP (binaire) en ligne (string)
-     function Conv_IP_ligne (IP: in T_Adresse_IP) return String;
+     function Conv_IP_String (IP: in T_Adresse_IP) return String;
 
      -- Lit l'adresse IP dans le fichier et la renvoie
      function Lire_Adresse_IP (Entree : File_Type) return T_Adresse_IP;
 
      -- Dis si un paquet correspond
      function Comp_Destination_Paquet(Destination:T_Adresse_IP; Masque: T_Adresse_IP; Paquet:T_Adresse_IP) return Boolean;
-
-
      
-
 end Adresse_IP;
