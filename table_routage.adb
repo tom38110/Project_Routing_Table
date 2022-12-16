8f09b4c51af49a7f6872f52dcf537f03aa2433c9
Mathieu-MALAK <114735678+Mathieu-MALAK@users.noreply.github.com>
2022-12-16 15:47:46 +0100

    spécification

--- table_routage.adb
+++ table_routage.adb
@@ -7,7 +7,14 @@ package body Table_Routage is
         procedure Free is
            new Ada.Unchecked_Deallocation (Object => T_Cellule, Name => T_Table_Routage);
 
-
+        --Ajoute le paquet (Destination, Masque, Interface_eth) à la table de routage
+        --Paramètres:
+        --      Destination  : l'adresse_IP du paquet
+        --      Masque       : le masque du paquet
+        --      Interface_eth: l'interface du paquet
+        --
+        --Précondition: aucune
+        --Postcondition: l'élément (Destination,Masque,Interface_eth) est le dernier élément de Table
         procedure Ajouter(Table : T_Table_Routage ; Destination: T_Adresse_IP ; Masque: T_Adresse_IP ; Interface_eth: Unbounded_String) is
                 Table_parcours : T_Table_Routage := Table;
         begin
@@ -18,7 +25,12 @@ package body Table_Routage is
                 Table_parcours := new T_Cellule'(Destination, Masque, Interface_eth, null);    
         end Ajouter;
 
-
+        --Vide la table de routage
+        --Paramètres:
+        --      Table : La table de routage à vider
+        --
+        --Précondition: aucune
+        --Postcondition: Table est vide
         procedure Vider(Table : T_Table_Routage) is
                 Detruire : T_Table_Routage ;
         begin                                               --On procede recursivement: a chaque appelle de la procedure,
@@ -33,13 +45,24 @@ package body Table_Routage is
         end Vider;
 
 
-
+        --Initialise la table de routage
+        --Paramètres:
+        --      Table : La table de routage à initialiser
+        --
+        --Précondition: aucune
+        --Postcondition: Table est initialisée à null
         procedure Initialiser(Table : T_Table_Routage) is
         begin
                 Table := null;
         end Initialiser;
 
-
+        --Retourne l'interface correspondant au paquet à l'aide de Table
+        --Paramètres:
+        --      Table  : La table de routage contenant l'interface
+        --      Paquet : Le paquet à router avec l'interface que retourne cette fonction
+        --
+        --Précondition: aucune
+        --Postcondition: L'interface doit correspondre au paquet
         function Chercher_Element(Table : T_Table_Routage ; Paquet: T_Adresse_IP) return Unbounded_String is
                 Table_parcours : T_Table_Routage := Table;
         begin
@@ -50,7 +73,12 @@ package body Table_Routage is
         end Chercher_Element;
 
 
-
+        --Affiche la table de routage
+        --Paramètres:
+        --      Table : La table de routage à afficher
+        --
+        --Précondition: aucune
+        --Postcondition: Table a entièrement été affiché
         procedure Afficher(Table : T_Table_Routage) is
                 Table_parcours : T_Table_Routage := Table;
         begin
