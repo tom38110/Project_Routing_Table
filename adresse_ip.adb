1f49e78b98b847e2be2651e2bb0a6fe4850b7e37
tom38110 <75202875+tom38110@users.noreply.github.com>
2022-12-17 14:58:59 +0100

    Update adresse_ip.adb

--- adresse_ip.adb
+++ adresse_ip.adb
@@ -12,7 +12,7 @@ package body Adresse_IP is
         IP := Adresse_IP + 2 ** 8;
 
           for i in 1..4 loop                                                          -- on boucle 4 fois pour afficher toutes les valeurs dans une adresse IP
-               Str:= Str &  Integer'Image (((IP / UN_OCTET ** (4-i)) mod UN_OCTET));
+               Str:= Str &  Integer'Image (Natural(((IP / UN_OCTET ** (4-i)) mod UN_OCTET)));
                while i/=4 loop                                                             -- permet d'afficher les "." juste après les 3 premières valeurs
                     Str:= Str & ".";
                end loop;
@@ -23,7 +23,7 @@ package body Adresse_IP is
 
      -- Fonction qui permet de lire une adresse IP dans un fichier Entree et la convertir en un type T_Adresse_IP
     
-     function Lire_Adresse_IP (Entree : File_Type) return T_Adresse_IP is 
+     function Lire_Adresse_IP (Entree : in File_Type) return T_Adresse_IP is 
          
           c: character;                     -- permet de traiter le "."
           val:Integer;                      -- la valeur (ni) qui va etre convertie en binaire
@@ -34,7 +34,7 @@ package body Adresse_IP is
           for i in 1..4 loop                -- pour chaque ni de l'adresse IP
                Get(Entree,val);             -- on récupère la valeur de ni
                Get(Entree,c);               -- on lit le "." pour éviter de stopper l'algorithme
-               ad_ip := ad_ip*UN_OCTET + val;       -- on construit l'adresse_IP à l'aide du schéma de Horner
+               ad_ip := ad_ip*UN_OCTET + Integer'Image (val);       -- on construit l'adresse_IP à l'aide du schéma de Horner
           end loop;
           return ad_ip;                     -- on renvoie l'adresse IP convertie en binaire
      
@@ -42,28 +42,26 @@ package body Adresse_IP is
 
      -- Fonction qui renvoie un booléen qui confirme ou non la compatibilité entre une Destination et un Paquet.
 
-     function Comp_Destination_Paquet(Destination:T_Adresse_IP; Masque: T_Adresse_IP; Paquet:T_Adresse_IP) return Boolean is 
+     function Comp_Destination_Paquet(Destination: in T_Adresse_IP; Masque: in T_Adresse_IP; Paquet: in T_Adresse_IP) return Boolean is 
           
           val2:T_Adresse_IP;
           T:Boolean;
           M:T_Adresse_IP;
           i:Integer;
-          Dest2: T_Adresse_IP;
+          
      begin
      
           T:=True;                          -- on initialise notre T à True 
           val2 := Paquet;                   -- on créé une copie du paquet
           M := Masque;                      -- on créé une copie du masque 
           i:=0;
-          Dest2:=Destination;
-          
-          while T=True and then i < 4 loop                      -- tant qu'on a bien égalité entre les valeurs de destination et de paquet et que l'on n'est pas arrivé à la fin
-               if (Natural(val2 mod UN_OCTET)) = (Natural(Destination mod UN_OCTET)) then -- si on a egalité entre les 2 ni on ne fait rien
+          while T and then i < 4 loop                      -- tant qu'on a bien égalité entre les valeurs de destination et de paquet et que l'on n'est pas arrivé à la fin
+               if (val2 mod UN_OCTET) = (Destination mod UN_OCTET) then -- si on a egalité entre les 2 ni on ne fait rien
                     null;
                else                                                 
-                    if (Natural(masque mod UN_OCTET)) = 0 then               -- sinon on observe la valeur du masque associée, si elle est égale à 0 on ne fait rien
+                    if (masque mod UN_OCTET) = 0 then               -- sinon on observe la valeur du masque associée, si elle est égale à 0 on ne fait rien
                          val2:= val2 / UN_OCTET;                    -- on modifie les valeurs des copies afin de passer aux ni suivants
-                         Dest2 := Dest2 / UN_OCTET;
+                         Destination := Destination / UN_OCTET;
                          M:= M / UN_OCTET;
                     else
                          T:=False;                                  -- on change T en False car on ne respecte pas la cohérence du cache.
@@ -76,4 +74,3 @@ package body Adresse_IP is
      end Comp_Destination_Paquet;
           
 end Adresse_IP;
-          
