ffe73c297176517b72bbd06ebf9b76631091ac4f
tom38110 <75202875+tom38110@users.noreply.github.com>
2022-12-25 13:17:26 +0100

    Update table_routage.ads

--- table_routage.ads
+++ table_routage.ads
@@ -1,11 +1,5 @@
-with Adresse_IP;  use Adresse_IP;
-with Ada.Text_IO;                  use Ada.Text_IO;
-with Ada.Integer_Text_IO;          use Ada.Integer_Text_IO;
-with Ada.Command_Line;             use Ada.Command_Line;
 with Ada.Strings;                  use Ada.Strings;
 with Ada.Strings.Unbounded;        use Ada.Strings.Unbounded;
-with Ada.Text_IO.Unbounded_IO;     use Ada.Text_IO.Unbounded_IO;
-with Ada.Exceptions;               use Ada.Exceptions;
 with Adresse_IP;                   use Adresse_IP;
 
 package Table_Routage is
@@ -16,20 +10,20 @@ package Table_Routage is
         type T_Politique is (FIFO, LRU, LFU);
 
         --Ajouter l'adresseIP, le masque et l'interface dans la table de routage
-        procedure Ajouter(Table : T_Table_Routage ; Destination: T_Adresse_IP ; Masque: T_Adresse_IP ; Interface_eth: Unbounded_String);
+        procedure Ajouter(Table : in out T_Table_Routage ; Destination : in T_Adresse_IP ; Masque : in T_Adresse_IP ; Interface_eth : in Unbounded_String);
 
         --Supprimer tous les elements de la table
-        procedure Vider(Table : T_Table_Routage);
+        procedure Vider(Table : in out T_Table_Routage);
 
         --Pointer la table de routage vers null
-        procedure Initialiser(Table : T_Table_Routage);
+        procedure Initialiser(Table : out T_Table_Routage);
 
         --Parcourir la liste chainee jusqu'a trouver l'element ayant cette adresseIP avec un masque valide
         --Retouner l'interface de cet element
-        function Chercher_Element(Table : T_Table_Routage ; Paquet: T_Adresse_IP) return Unbounded_String;
+        function Chercher_Element(Table : in T_Table_Routage ; Paquet: in T_Adresse_IP) return Unbounded_String;
 
         --Afficher chaque ligne de la Table de Routage
-        procedure Afficher(Table : T_Table_Routage);
+        procedure Afficher(Table : in out T_Table_Routage);
 
 private
 
