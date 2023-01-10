cad334db7a605ccf4ba0cd6f674becdb62bb1c11
tom38110 <tom.audard@gmail.com>
2023-01-10 23:11:49 +0100

    Cohérence gérée et peut être bien opti

--- table_routage.adb
+++ table_routage.adb
@@ -39,30 +39,7 @@ new Ada.Unchecked_Deallocation (Object => T_Cellule, Name => T_Table_Routage);
         end Initialiser;
 
 
-        procedure Chercher_Interface(Table : in T_Table_Routage ; Paquet : in out T_Adresse_IP ; Interface_eth : out Unbounded_String ; Cache : in out T_Cache ; Capacite_Cache : in Integer ; Politique : in T_Politique) is
-                Table_parcours : T_Table_Routage := Table;
-                Masque_Max : T_Adresse_IP := 0;
-                Destination : T_Adresse_IP;
-        begin
-                -- Trouver l'interface correspondant au paquet
-                while Table_parcours /= Null loop
-                        if Comp_Destination_Paquet(Table_parcours.all.Destination, Table_parcours.all.Masque, Paquet) and then Masque_Max < Table_parcours.all.Masque then
-                                Interface_eth := Table_parcours.all.Interface_eth;
-                                Masque_Max := Table_parcours.all.Masque;
-                                Destination := Table_parcours.all.Destination;
-                        else
-                                Null;
-                        end if;
-                        Table_parcours := Table_parcours.all.Suivante;
-                end loop;
-                -- Ajouter la ligne utilisée au cache en respectant la cohérence
-                Masque_Max := Gerer_Coherence_Cache(Table, Destination, Masque_Max);
-                Paquet := Paquet and Masque_Max;
-                Maj_Cache(Cache, Capacite_Cache, Politique, Masque_Max, Interface_eth, Paquet);
-        end Chercher_Interface;
-
-
-        -- Renvoie un masque qui permettra de respecter la cohérence du cache (masque le plus long parmis les routes qui correspondent à la route utilisée)
+ -- Renvoie un masque qui permettra de respecter la cohérence du cache (masque le plus long parmis les routes qui correspondent à la route utilisée)
         function Gerer_Coherence_Cache(Table : in T_Table_Routage ; Destination : in T_Adresse_IP ; Masque : in T_Adresse_IP) return T_Adresse_IP is
                 Masque_coherent : T_Adresse_IP := Masque; -- Masque qui respecte la cohérence du cache
                 Table_Parcours : T_Table_Routage := Table; -- Curseur qui parcours la table
@@ -87,8 +64,8 @@ new Ada.Unchecked_Deallocation (Object => T_Cellule, Name => T_Table_Routage);
                 Ie_bit : Integer;
         begin
                 while Table_parcours /= Null loop
-                        if Comp_Destination_Paquet(Destination, Masque, Table_parcours.all.Destination) and then Masque_coherent < Table_parcours.all.Masque then
-                                Ie_bit := Trouver_Ie_Bit_Diff(Destination_correspondante);
+                        if Destination /= Table_parcours.all.Destination and then Comp_Destination_Paquet(Destination, Masque, Table_parcours.all.Destination) and then Masque_coherent < Table_parcours.all.Masque then
+                                Ie_bit := Trouver_Ie_Bit_Diff(Destination_correspondante, Table_parcours.all.Destination);
                                 Mise_A_1(Masque_coherent, Ie_bit + 1);
                                 Destination_correspondante := Table_parcours.all.Destination;
                         else
@@ -98,6 +75,29 @@ new Ada.Unchecked_Deallocation (Object => T_Cellule, Name => T_Table_Routage);
                 end loop;
                 return Masque_coherent;
         end Gerer_Coherence_Cache;
+
+
+        procedure Chercher_Interface(Table : in T_Table_Routage ; Paquet : in out T_Adresse_IP ; Interface_eth : out Unbounded_String ; Cache : in out T_Cache ; Capacite_Cache : in Integer ; Politique : in T_Politique) is
+                Table_parcours : T_Table_Routage := Table;
+                Masque_Max : T_Adresse_IP := 0;
+                Destination : T_Adresse_IP;
+        begin
+                -- Trouver l'interface correspondant au paquet
+                while Table_parcours /= Null loop
+                        if Comp_Destination_Paquet(Table_parcours.all.Destination, Table_parcours.all.Masque, Paquet) and then Masque_Max < Table_parcours.all.Masque then
+                                Interface_eth := Table_parcours.all.Interface_eth;
+                                Masque_Max := Table_parcours.all.Masque;
+                                Destination := Table_parcours.all.Destination;
+                        else
+                                Null;
+                        end if;
+                        Table_parcours := Table_parcours.all.Suivante;
+                end loop;
+                -- Ajouter la ligne utilisée au cache en respectant la cohérence
+                Masque_Max := Gerer_Coherence_Cache(Table, Destination, Masque_Max);
+                Paquet := Paquet and Masque_Max;
+                Maj_Cache(Cache, Capacite_Cache, Politique, Masque_Max, Interface_eth, Paquet);
+        end Chercher_Interface;
 
 
         procedure Afficher(Table : in T_Table_Routage) is
